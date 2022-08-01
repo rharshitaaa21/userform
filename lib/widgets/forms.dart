c224c96c0d24242b27c89c98c04ca9ded561e8e0
Harshita Raghav <raghav.harshita21@gmail.com>
2022-08-01 18:55:32 +0530

    Updates

--- lib/widgets/forms.dart
+++ lib/widgets/forms.dart
@@ -18,8 +18,9 @@ class _FormsState extends State<Forms> {
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   _LoginData _data = new _LoginData();
   final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _ageController = TextEditingController();
-  final TextEditingController _classController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _mailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   String _validateEmail(String value) {
     try {
@@ -52,43 +53,51 @@ class _FormsState extends State<Forms> {
       body: Form(
         key: _formkey,
         child: ListView(children: <Widget>[
-          Card(
-            child: TextFormField(
-              keyboardType: TextInputType.name,
-              decoration:
-                  const InputDecoration(hintText: 'Name', labelText: 'Name'),
-              controller: _nameController,
-            ),
+          TextFormField(
+            keyboardType: TextInputType.name,
+            decoration:
+                const InputDecoration(hintText: 'Name', labelText: 'Name'),
+            controller: _nameController,
           ),
-          Card(
-            child: TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                hintText: 'Age',
-              ),
-              controller: _ageController,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please Enter Valid Text';
-                }
-                return value;
-              },
+          TextFormField(
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              hintText: 'Email',
             ),
+            controller: _mailController,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please Enter Valid Text';
+              }
+              return value;
+            },
           ),
-          Card(
-            child: TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: 'E-mail Address',
-              ),
-              controller: _classController,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please Enter Valid Text';
-                }
-                return value;
-              },
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Phone',
             ),
+            controller: _phoneController,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please Enter Valid Text';
+              }
+              return value;
+            },
+          ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: 'Password',
+            ),
+            controller: _passwordController,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please Enter Valid Text';
+              }
+              return value;
+            },
           ),
           ElevatedButton(
             onPressed: submit,
